56a034f34416716f5d9bc395c6a9ff0d39556dd7
Gerigon <Rutger-100@hotmail.com>
2015-11-30 17:52:57 +0100

    Basic Ship + Canon :earth_africa:

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public GameObject cannon;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.GetAxis("Horizontal")!= 0)
+        {
+            cannon.transform.Rotate(Vector3.up, Input.GetAxis("Horizontal"),Space.World);
+        }
+	}
+}
