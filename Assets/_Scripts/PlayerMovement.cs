a470d084ebf0eb38e0c36706ec81156ee53ed5fb
flyzza <zfy1995@hotmail.com>
2015-12-03 11:51:31 +0100

    pathfinding A* & basic movement

---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float moveSpeed = 10;
+    public float turnSpeed = 5;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Movement(v);
+        Rotation(h);
+    }
+
+    //movement of the player
+    void Movement(float v)
+    {
+        Vector3 movement = new Vector3(0.0f, 0.0f, v);
+        transform.Translate(movement * moveSpeed * Time.deltaTime);
+    }
+
+    void Rotation(float h)
+    {
+        Vector3 rotation = new Vector3(0.0f, h, 0.0f);
+        transform.Rotate(rotation * turnSpeed * Time.deltaTime);
+    }
+}
