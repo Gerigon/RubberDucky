0ce5fd94f8953bfd6c58c5f94fda350a76a635c6
flyzza <zfy1995@hotmail.com>
2016-01-25 22:04:58 +0100

    Sprites and start pop up

---
+++ Assets/Scripts/PopUpUI.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PopUpUI : MonoBehaviour {
+
+    public Camera boatCamera;
+    public Canvas thisCanvas;
+
+	// Use this for initialization
+	void Start ()
+    {
+        boatCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        thisCanvas = transform.GetComponent<Canvas>();
+        thisCanvas.worldCamera = boatCamera;
+
+        thisCanvas.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+
+    public void EnableCanvas()
+    {
+        thisCanvas.enabled = true;
+    }
+
+    public void DisableCanvas()
+    {
+        thisCanvas.enabled = false;
+    }
+
+    public void LoadImage(GameObject thisGameObject)
+    {
+        switch(thisGameObject.name)
+        {
+            case "Super Hull";
+        }
+    }
+}
---
+++ Assets/Scripts/RendererFix.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RendererFix : MonoBehaviour {
+
+    public int queueValue = 1;
+
+	// Use this for initialization
+	void Start () {
+        Renderer rend = GetComponent<Renderer>();
+        rend.material.renderQueue = queueValue;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
